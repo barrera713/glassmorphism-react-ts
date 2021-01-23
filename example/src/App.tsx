8a829fc87dd53bb883bb145b75192504e582bbba
carlos barrera <dev.carlos96@gmail.com>
2021-01-22 23:57:19 -0600

    added padding to display example buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,9 +7,17 @@ const App = () => {
   return (
     <div className="dev-background">
       <h5>Glassmorphism</h5>
-      <Box  />
+      <Box />
       <p>Buttons</p>
+      <div style={{padding: '10px 0'}}>
+      <Button size='sm'>Small</Button>
+      </div>
+      <div style={{padding: '10px 0'}}>
       <Button>Normal</Button>
+      </div>
+      <div style={{padding: '10px 0'}}>
+      <Button size='lg'>Normal</Button>
+      </div>
     </div>
   )
 };
