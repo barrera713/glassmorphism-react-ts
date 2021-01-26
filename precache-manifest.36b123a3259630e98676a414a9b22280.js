c52eedff699b80d0ff14a7e4af43e3dd9e78c0de
carlos barrera <dev.carlos96@gmail.com>
2021-01-25 22:11:59 -0600

    Updates

---
+++ precache-manifest.36b123a3259630e98676a414a9b22280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed9ade682ccf85ca7075401c5ed2b02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acce4c1ed5b988d82abf",
+    "url": "./static/css/main.ffa009db.chunk.css"
+  },
+  {
+    "revision": "b43ad6180dc88495fc42",
+    "url": "./static/js/2.0f50894e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0f50894e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acce4c1ed5b988d82abf",
+    "url": "./static/js/main.0abbd3b5.chunk.js"
+  },
+  {
+    "revision": "9cc38f526c7cb2bf22ba",
+    "url": "./static/js/runtime-main.5a33bce7.js"
+  }
+]);
